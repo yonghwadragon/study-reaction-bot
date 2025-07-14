3be0858570f1a5280409a67032e8eca82b4c10d9
Yonghwa Song <157938795+yonghwadragon@users.noreply.github.com>
2025-07-14 22:56:24 +0900

    Update events.py

--- api/slack/events.py
+++ api/slack/events.py
@@ -5,14 +5,10 @@ from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-@app.post("/api/slack/events")
+@app.post("/")
 async def slack_events(request: Request):
     data = await request.json()
-
-    # URL 검증 (Slack Event Subscription 초기 인증)
     if data.get("type") == "url_verification":
         challenge = data.get("challenge")
         return JSONResponse(content={"challenge": challenge})
-
-    # 이후 Slack event 처리 영역
     return JSONResponse(content={})
